395e27b93d2486cd2407f3da49f0320f61960d7d
Jitender <jitender.jk54@gmail.com>
2021-02-10 17:57:39 +0530

    correct nodemon

--- server.js
+++ server.js
@@ -36,8 +36,11 @@ app.get('/', (req, res)=>{
     res.render("index");
 });
 
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT;
+if(PORT == "" || PORT == null){
+    PORT = 3000;
+}
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`);
-    console.log(`Click on this link: http://localhost:${PORT}`);
+    //console.log(`Click on this link: http://localhost:${PORT}`);
 });
